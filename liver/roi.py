28de471a3ec3e7ddf10424bf380a4a5fed6c926d
avivd <avivdodi@gmail.com>
2020-12-03 11:43:37 +0200

    utils file added and roi functions progression

--- liver/roi.py
+++ liver/roi.py
@@ -1,40 +1,128 @@
 import nibabel as nib
 import numpy as np
-from skimage.morphology import label
-from skimage.morphology import remove_small_objects
-
+from skimage.morphology import label, remove_small_objects, convex_hull_image, convex_hull_object
+import utils
 
 
 def IsolateBody(ct_scan):
-
     ct_arr = ct_scan.get_fdata()
 
     # th
-    ct_arr[ct_arr < 500] = ?
-    ct_arr[ct_arr > 2000] = ?
 
-    #clean noise
+    # ct_arr[ct_arr >= -500] = 1
+    # ct_arr[ct_arr > 2000] = 0
+    # ct_arr[ct_arr < -500] = 0
+
+    ct_arr[(ct_arr > -500) & (ct_arr < 2000)] = 1
+    ct_arr[(ct_arr <= -500) | (ct_arr >= 2000)] = 0
+
+    # clean noise
     ct_arr = label(ct_arr)
-    ct_arr = remove_small_objects(ct_arr, min_size=50, connectivity=24)
+    ct_arr = remove_small_objects(ct_arr, min_size=200, connectivity=24)
 
     # compute the largest connected component
+    ct_arr = label(ct_arr)
+    assert (ct_arr.max() != 0)  # assume at least 1 CC
+    body_seg = ct_arr == np.argmax(np.bincount(ct_arr.flat)[1:]) + 1
+
+    # new_nifti = nib.Nifti1Image(body_seg.astype(np.float), ct_scan.affine)
+    # nib.save(new_nifti, f'test.nii.gz')
 
     return body_seg
 
 
-def IsolateBS(body_seg):
+def IsolateBS(body_seg, aff):
     # find 2 large cavities
 
-    #the plane BB corresponds to the inferior slice of the lungs; the plane CC corresponds to the widest slice of the lungs
-    # The plane CC is the last slice/slice in which the lungs slice does not change much (or close
-    # to it).
+    # find the seg of air
+    lungs_seg = 1 - body_seg
 
-    return lungs_seg, bb, cc
+    # remove the air outside the body (upper)
+    lungs_seg = label(lungs_seg)
+    air = lungs_seg == np.argmax(np.bincount(lungs_seg.flat)[1:]) + 1
+    lungs_seg -= air
+
+    # remove the air outside the body (lower)
+    lungs_seg = label(lungs_seg)
+    air = lungs_seg == np.argmax(np.bincount(lungs_seg.flat)[1:]) + 1
+    lungs_seg -= air
+
+    # take only the lungs
+    lungs_seg = label(lungs_seg)
+    lungs_seg = lungs_seg == np.argmax(np.bincount(lungs_seg.flat)[1:]) + 1
+
+    # new_nifti = nib.Nifti1Image(lungs_seg.astype(np.float), aff)
+    # nib.save(new_nifti, f'test.nii.gz')
+
+    xmin, xmax, ymin, ymax, zmin, zmax = utils.compute_seg_boundary_inds(lungs_seg)
+
+    # inferior slice of lungs.
+    bb = zmin
+    # widest slice of lungs amond y axis (sagittal view)
+    cc = utils.compute_widest_slice(lungs_seg)
+
+    return lungs_seg, cc, bb
 
 
-def ThreeDBand (body_seg, lungs_seg, bb, cc):
+def ThreeDBand(body_seg, lungs_seg, bb, cc):
+    band = body_seg[:, :, bb:cc]
+    band = np.zeros(body_seg.shape, dtype=np.uint8)
+    for i in range(1, body_seg.shape[2]):
+        if bb <= i <= cc:
+            band[:, :, i] = body_seg[:, :, i]
+
+    print(band)
+    # aff = nib.load('/cs/casmip/public/for_aviv/MedicalImageProcessing/liver/test.nii.gz').affine
+    # new_nifti = nib.Nifti1Image(band.astype(np.float), aff)
+    # nib.save(new_nifti, f'band.nii.gz')
+    return band
 
 
+def MergedROI(ct_scan):
+    ct_arr = ct_scan.get_fdata()
+    # aorta gt
+    # spine roi
+
+    filename = ''
+    new_nifti = nib.Nifti1Image(band.astype(np.float), ct_scan.affine)
+    nib.save(new_nifti, f'{filename}_ROI.nii.gz')
+
+    # return roi
 
 
-    return lungs_band
+def liver_roi(body_seg, ct_scan):
+    ct_arr = ct_scan.get_fdata()
+
+    # th
+    ct_arr[(ct_arr > -100) & (ct_arr < 200)] = 1
+    ct_arr[(ct_arr <= -100) | (ct_arr >= 200)] = 0
+    intersection = np.logical_and(body_seg, ct_arr)
+
+    # find half of the aorta
+    # find the largest label there
+
+    # save
+
+    return
+
+
+def findSeeds(ct_scan, roi):
+    return seeds_list
+
+
+def multipleSeedsRG(ct_scan, roi):
+    return liver_seg
+
+
+def segmentLiver(ct_nii, aorta_nii, output_name):
+    new_nifti = nib.Nifti1Image(liver_seg.astype(np.float), ct_nii.affine)
+    nib.save(new_nifti, f'{output_name}_LiverSeg.nii.gz')
+
+    return liver_seg
+
+
+if __name__ == '__main__':
+    load = nib.load('/cs/casmip/public/for_aviv/MedicalImageProcessing/Targil1_data/Case2_CT.nii.gz')
+    body_seg = IsolateBody(load)
+    lungs_seg, cc, bb = IsolateBS(body_seg, load.affine)
+    lungs_band = ThreeDBand(body_seg, lungs_seg, bb, cc)
---
+++ liver/utils.py
@@ -0,0 +1,75 @@
+import nibabel as nib
+import numpy as np
+
+
+def compute_seg_boundary_inds(img):
+    """
+    Calculate the bbox of a seg.
+    :param img:
+    :return: xmin, xmax, ymin, ymax, zmin, zmax indexes
+    """
+    x = np.any(img, axis=(1, 2))
+    y = np.any(img, axis=(0, 2))
+    z = np.any(img, axis=(0, 1))
+
+    xmin, xmax = np.where(x)[0][[0, -1]]
+    ymin, ymax = np.where(y)[0][[0, -1]]
+    zmin, zmax = np.where(z)[0][[0, -1]]
+
+    return xmin, xmax, ymin, ymax, zmin, zmax
+
+
+def check_orientation(ct_image, ct_arr):
+    """
+    Check the NIfTI orientation, and flip to  'RPS' if needed.
+    :param ct_image: NIfTI file
+    :param ct_arr: array file
+    :return: array after flipping
+    """
+    x, y, z = nib.aff2axcodes(ct_image.affine)
+    if x != 'R':
+        ct_arr = nib.orientations.flip_axis(ct_arr, axis=0)
+    if y != 'P':
+        ct_arr = nib.orientations.flip_axis(ct_arr, axis=1)
+    if z != 'S':
+        ct_arr = nib.orientations.flip_axis(ct_arr, axis=2)
+    return ct_arr
+
+
+def evaluateSegmentation(est_seg, zmin, zmax, gt_data, name):
+    """
+    compute the dice and vod.
+    :param est_seg: seg to compute
+    :param zmin: lower z value of the L1.
+    :param zmax: upper z value of the L1
+    :param gt_data: Ground Truth seg file
+    :param name: case name
+    :return: tuple of VOD and DICE
+    """
+
+    seg1_data = gt_data[:, :, zmin:zmax]
+    seg2_data = est_seg[:, :, zmin:zmax]
+    intersection = np.logical_and(seg1_data, seg2_data)
+    union = np.logical_or(seg1_data, seg2_data)
+
+    dice_result = (2 * intersection.sum()) / (seg1_data.sum() + seg2_data.sum())
+    vod_result = 1 - (intersection.sum() / union.sum())
+
+    print(f'Case {name} VOD, DICE: {np.round(vod_result, 3)} , {np.round(dice_result, 3)}')
+    return vod_result, dice_result
+
+
+def compute_widest_slice(img):
+    """
+    Compute the z index of the widest sagittal view (y axis).
+    :param img: The nii array.
+    :return: Z key slice of the y widest axis (sagittal)
+    """
+    y_sum = np.sum(img, axis=1)
+    z_index = np.argmax(y_sum, axis=0)
+    return np.argmax(z_index)
+
+
+if __name__ == '__main__':
+    nii = nib.load('/cs/casmip/public/for_aviv/MedicalImageProcessing/liver/test.nii.gz')
+    compute_widest_slice(nii.get_fdata())
