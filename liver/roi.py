ed0cb9cf4e5792c3c1bdd09820ed13bed1bd1a4f
AvivD <avivdodi@gmail.com>
2020-12-05 23:03:07 +0200

    Functions added in roi.py

--- liver/roi.py
+++ liver/roi.py
@@ -5,14 +5,14 @@ import utils
 
 
 def IsolateBody(ct_scan):
+    """
+    Body segmentation of all the body, without air.
+    :param ct_scan:
+    :return:Body segmentation
+    """
     ct_arr = ct_scan.get_fdata()
 
-    # th
-
-    # ct_arr[ct_arr >= -500] = 1
-    # ct_arr[ct_arr > 2000] = 0
-    # ct_arr[ct_arr < -500] = 0
-
+    # filter th between -500 to 2000
     ct_arr[(ct_arr > -500) & (ct_arr < 2000)] = 1
     ct_arr[(ct_arr <= -500) | (ct_arr >= 2000)] = 0
 
@@ -31,8 +31,12 @@ def IsolateBody(ct_scan):
     return body_seg
 
 
-def IsolateBS(body_seg, aff):
-    # find 2 large cavities
+def IsolateBS(body_seg):
+    """
+    Isolate the lungs from the body
+    :param body_seg:
+    :return: lungs segmentation, inferior slice, widest slice of the lungs (sagittal view)
+    """
 
     # find the seg of air
     lungs_seg = 1 - body_seg
@@ -58,13 +62,22 @@ def IsolateBS(body_seg, aff):
 
     # inferior slice of lungs.
     bb = zmin
-    # widest slice of lungs amond y axis (sagittal view)
+    # widest slice of lungs among y axis (sagittal view)
     cc = utils.compute_widest_slice(lungs_seg)
 
     return lungs_seg, cc, bb
 
 
 def ThreeDBand(body_seg, lungs_seg, bb, cc):
+    # todo fix the function.
+    """
+
+    :param body_seg:
+    :param lungs_seg:
+    :param bb:
+    :param cc:
+    :return:
+    """
     band = body_seg[:, :, bb:cc]
     band = np.zeros(body_seg.shape, dtype=np.uint8)
     for i in range(1, body_seg.shape[2]):
@@ -78,8 +91,14 @@ def ThreeDBand(body_seg, lungs_seg, bb, cc):
     return band
 
 
-def MergedROI(ct_scan):
+def MergedROI(ct_scan, aorta_arr):
+    # fixme
     ct_arr = ct_scan.get_fdata()
+    img = np.zeros(ct_arr.shape, dtype=np.uint8)
+
+    for i in range(1, ct_arr.shape[2]):
+        curr_slice = ct_arr[..., i]
+
     # aorta gt
     # spine roi
 
@@ -90,8 +109,11 @@ def MergedROI(ct_scan):
     # return roi
 
 
-def liver_roi(body_seg, ct_scan):
+def liver_roi(body_seg, ct_scan, aorta_nii):
+    # fixme
+
     ct_arr = ct_scan.get_fdata()
+    aorta_arr = aorta_nii.get_fdata()
 
     # th
     ct_arr[(ct_arr > -100) & (ct_arr < 200)] = 1
@@ -99,22 +121,54 @@ def liver_roi(body_seg, ct_scan):
     intersection = np.logical_and(body_seg, ct_arr)
 
     # find half of the aorta
+
     # find the largest label there
 
+    ct_label = label(intersection)
+    liver_seg = ct_label == np.argmax(np.bincount(ct_label.flat)[1:]) + 1
+
+    # todo check this func by save the nifti. maybe we need to use the aorta seg and location.
     # save
 
-    return
+    return roi
 
 
-def findSeeds(ct_scan, roi):
+def findSeeds(ct_scan, roi, seeds_num=200):
+    """
+    Find sample seeds from roi on ct_scan.
+    :param ct_scan: The scan
+    :param roi: The segmentation to search for seeds.
+    :param seeds_num: number of seeds.
+    :return: seeds list from the roi on the scan.
+    """
+    seeds_list = []
+    ct_arr = ct_scan.get_fdata()
+    points = np.argwhere(roi)
+    np.random.shuffle(points)
+
+    for ind in points[0:seeds_num]:
+        seeds_list.append(ct_arr[ind[0], ind[1], ind[2]])
+
     return seeds_list
 
 
 def multipleSeedsRG(ct_scan, roi):
+    # todo fixme
+    seeds_list = findSeeds(ct_scan, roi)
+
+    # Perform Seeded Region Growing with N initial points
+
     return liver_seg
 
 
 def segmentLiver(ct_nii, aorta_nii, output_name):
+    # todo fix this function
+    ct_scan = ct_nii.get_fdata()
+
+    body_seg = IsolateBody(ct_scan)
+
+    roi = liver_roi(body_seg, ct_scan, aorta_nii)
+    liver_seg = multipleSeedsRG(ct_scan, roi)
     new_nifti = nib.Nifti1Image(liver_seg.astype(np.float), ct_nii.affine)
     nib.save(new_nifti, f'{output_name}_LiverSeg.nii.gz')
 
